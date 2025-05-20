8d103fd911fb6e1f0345c7b9f6824dace5706853
Kanyawatchalermthai <148806112+Kanyawatchalermthai@users.noreply.github.com>
2025-05-20 21:36:12 +0700

    Delete BE directory

--- BE/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-# Create Blueprint for API
-api_bp = Blueprint('api', __name__)
-
-# Import routes
-from .routes import comment
-from .routes import user
-from .routes import product
